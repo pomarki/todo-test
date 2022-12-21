ba0c527934186534565f9ff1cdf0574508cd6c8e
Denis Shalaev <pomarki@narod.ru>
2022-12-21 13:54:39 +0300

    02

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,14 +1,36 @@
-import "./Item.css"
+import "./Item.css";
 
-function Item ({ type }) {
-    return (
-        <div className="item__conteiner">
-         <div className="item_title">
-            <button className="item__delete"></button>
-            
-         </div>
+function Item({ type }) {
+  return (
+    <form className="item__conteiner">
+      <div className="item__title-conteiner">
+        <button className="item__delete-button" type="button"></button>
+        <input type="text" className="item__title" placeholder="заголовок" />
+      </div>
+      <div className="item__sub-conteiner">
+        <div className="item__data-container">
+          <button className="item__data-button" type="button"></button>
+          <input
+            type="data"
+            className="item__data-input"
+            placeholder="дата окончания"
+          />
         </div>
-    )
+        <div className="item__option-conteiner">
+          <button className="item__save-button"></button>
+          <button className="item__add-button"></button>
+        </div>
+        <div className="item__done-container">
+          <p className="item__subtitle">завершено</p>
+          <button className="item__done-button"></button>
+        </div>
+      </div>
+      <textarea
+        className="item__description"
+        placeholder="описание задания"
+      ></textarea>
+    </form>
+  );
 }
 
 export default Item;
