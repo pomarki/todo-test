0c65b0cfd475c1297dd3c162f0986f9cbb634b21
Denis Shalaev <pomarki@narod.ru>
2022-12-22 16:03:38 +0300

    02

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -4,27 +4,27 @@ function Item({ type }) {
   return (
     <form className="item__conteiner">
       <div className="item__title-conteiner">
-        <button className="item__delete-button" type="button"></button>
-        <input type="text" className="item__title" placeholder="заголовок" />
+        <button className="item__delete-button item__button item_form-el" type="button"></button>
+        <input type="text" className="item__title item_form-el" placeholder="заголовок" />
       </div>
       <div className="item__sub-conteiner">
         <input
-          className="item__data-input"
+          className="item__data-input item_form-el"
           placeholder="дата окончания"
           type="date"
         />
 
         <div className="item__option-conteiner">
-          <button className="item__save-button" type="button"></button>
-          <button className="item__add-button" type="button"></button>
+          <button className="item__save-button item__button_s" type="button"></button>
+          <button className="item__add-button item__button_s" type="button"></button>
         </div>
         <div className="item__done-container">
           <p className="item__subtitle">завершено</p>
-          <button className="item__done-button" type="button"></button>
+          <button className="item__done-button item__button item_form-el" type="button"></button>
         </div>
       </div>
       <textarea
-        className="item__description"
+        className="item__description item_form-el"
         placeholder="описание задания"
       ></textarea>
     </form>
